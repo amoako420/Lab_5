2d16a28bbb1cd7576459e8112c6ef21a0648edda
Heskey Amoako <heskey.fordjour@amalitech.com>
2024-11-27 17:48:48 +0000

    building still in process

---
+++ dags/api_key.py
@@ -0,0 +1,2 @@
+def api_key():
+    return (f47b7320ff3f8199243a2954fadeccd4)
---
+++ dags/weather_etl.py
@@ -0,0 +1,95 @@
+from airflow import DAG
+from airflow.providers.http.hooks.http import HttpHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.decorators import task
+from airflow.utils.dates import days_ago
+import requests
+import json
+from api_key import api_key
+
+
+POSTGRES_CONN_ID='postgres_default'
+city_name = 'munich'
+API_CONN_ID='openweather_api'
+API_key = api_key()
+
+default_args={
+    'owner':'amoako',
+    'start_date':days_ago(1)
+}
+
+with DAG(dag_id='weather_etl_pipeline',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dags:
+    
+    @task()
+    def extract_weather_data():
+        # Use HTTP Hook to get connection details from Airflow connection
+
+        http_hook=HttpHook(http_conn_id=API_CONN_ID,method='GET')
+
+        ## Build the API endpoint
+        ## https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+        endpoint=f'/data/2.5/weather?q={city_name}&appid={API_key}'
+
+        ## Make the request via the HTTP Hook
+        response=http_hook.run(endpoint)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to fetch weather data: {response.status_code}")
+
+
+    @task()
+    def transform_weather_data(weather_data):
+        #Transforming the data
+        current_weather = weather_data['current_weather']
+        transformed_data = {
+            'city': current_weather[''],
+            'temperature': current_weather['temperature'],
+            'windspeed': current_weather['windspeed'],
+            'winddirection': current_weather['winddirection'],
+            'weathercode': current_weather['weathercode']
+        }
+        return transformed_data
+    
+    @task()
+    def load_weather_data(transformed_data):
+        """Load transformed data into PostgreSQL."""
+        pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+        conn = pg_hook.get_conn()
+        cursor = conn.cursor()
+
+        # Create table if it doesn't exist
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS daily_weather (
+            temperature FLOAT,
+            windspeed FLOAT,
+            winddirection FLOAT,
+            weathercode INT,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """)
+
+        # Insert transformed data into the table
+        cursor.execute("""
+        INSERT INTO weather_data (latitude, longitude, temperature, windspeed, winddirection, weathercode)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """, (
+            transformed_data['latitude'],
+            transformed_data['longitude'],
+            transformed_data['temperature'],
+            transformed_data['windspeed'],
+            transformed_data['winddirection'],
+            transformed_data['weathercode']
+        ))
+
+        conn.commit()
+        cursor.close()
+
+    ## DAG Worflow- ETL Pipeline
+    weather_data= extract_weather_data()
+    transformed_data=transform_weather_data(weather_data)
+    load_weather_data(transformed_data)
